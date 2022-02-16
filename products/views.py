19fbaa23ae8db9037fe5d9053a2b32929ba2d4d2
StinaAxelsson <stina91@live.se>
2022-02-16 14:06:41 +0000

    add product list selector and javacript

--- products/views.py
+++ products/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect, reverse
 from django.db.models import Q
+from django.db.models.functions import Lower
 from django.contrib import messages
 from .models import Product, Category
 
@@ -21,7 +22,8 @@ def all_products(request):
             if sortkey == 'name':
                 sortkey = 'lower_name'
                 products = products.annotate(lower_name=Lower('name'))
-
+            if sortkey == 'category':
+                sortkey = 'category__name'
             if 'direction' in request.GET:
                 direction = request.GET['direction']
                 if direction == 'desc':
