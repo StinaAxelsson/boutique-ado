a6e348b12e93153f833298cf97dfb07216523036
StinaAxelsson <stina91@live.se>
2022-02-25 09:15:02 +0000

    add stipe

--- checkout/views.py
+++ checkout/views.py
@@ -15,6 +15,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51KWz8fKxesD1Tm7OtlyDyG9VAy4Yh5xyGOZw9B19Vf5KotzHajixqCPxYyvBeDpOwEo36JlBA8HZomerhH7Zsghd00XZ18xymT',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
